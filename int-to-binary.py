7fe66dcf1f483d1e1fdf93d1482bbf8e77379649
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-03-10 23:12:35 +0530

    Create int-to-binary.py

---
+++ int-to-binary.py
@@ -0,0 +1,11 @@
+#Convert the sum of numbers to binary
+def add_binary(a,b):
+    #your code here
+    res=a+b
+    new_bin='{0:b}'.format(res)
+    return new_bin
+
+a=2
+b=3
+res=add_binary(a,b)
+print(res)
